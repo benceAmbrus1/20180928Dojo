e74b94461e16175e161d1a1c5090f2a4ff959298
benceAmbrus1 <o.g.bence@gmail.com>
2018-09-28 10:34:42 +0200

    first

---
+++ 20180928DojoMonday/Rotate Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180928DojoMonday
+{
+    class Rotate_Array
+    {
+        public static int[] Rotate( int[] array, int counter)
+        {
+            while( counter > 0)
+            {
+
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ DojoMondayTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DojoMondayTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DojoMondayTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d09592a4-111f-4497-92c9-8f1b35f1f0c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
