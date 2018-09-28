d09f7bf16723278a31f20a4d84919fcd58667943
benceAmbrus1 <o.g.bence@gmail.com>
2018-09-28 12:19:03 +0200

    Rotate array first test done

--- DojoMondayTest/Rotate_Array_Test.cs
+++ DojoMondayTest/Rotate_Array_Test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using _20180928DojoMonday;
+using System.Linq;
 
 namespace DojoMondayTest
 {
@@ -8,22 +9,18 @@ namespace DojoMondayTest
     public class Rotate_Array_Test
     {
         [TestMethod]
-        public void RotateArrayTest_Valid() //rename method
+        public void RotateArrayTest_Valid()
         {
             // Arrange
-
-            int[] testArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int[] testArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int testCounter = 4;
-            int[] expected = new int[] {7, 8, 9, 10, 1, 2, 3, 4, 5, 6 };
+            int[] expected = new int[] { 7, 8, 9, 10, 1, 2, 3, 4, 5, 6 };
 
             // Act
-
             int[] actual = Rotate_Array.Rotate(testArray, testCounter);
 
             // Assert
-
-            Assert.AreEqual(expected, actual, "The 2 array are not the same");
-
+            Assert.AreEqual(true, actual.SequenceEqual(expected), "The 2 array are not the same");
         }
     }
 }
